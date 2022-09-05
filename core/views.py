6e2a5594ff9e125d12910186b8be5516004b8996
Sankhya Siddesh <siddeshsankhya@gmail.com>
2022-09-05 16:27:44 +0530

    m

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from product.models import Product
 
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.shortcuts import render, redirect
+
 # Create your views here.
+
 
 def frontpage(request):
     newest_products = Product.objects.all()[0:8]
@@ -9,6 +17,38 @@ def frontpage(request):
         'newest_products': newest_products,
     }
     return render(request, 'core/frontpage.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) or None
+        if form.is_valid():
+            username = request.POST.get('username')
+
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/signup.html', {'form': form, 'title': 'reqister here'})
+
+###################################################################################
+################login forms###################################################
+
+
+def Login(request):
+    if request.method == 'POST':
+
+        # AuthenticationForm_can_also_be_used__
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            form = login(request, user)
+            return redirect('/')
+    form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form, 'title': 'log in'})
 
 
 def contactpage(request):
--- simple_multivendor_site/urls.py
+++ simple_multivendor_site/urls.py
@@ -4,12 +4,17 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('login/', core_views.Login, name='login'),
+    path('logout/', core_views.frontpage, name='logout'),
+    path('register/', core_views.register, name='register'),
     path('vendor/', include('vendor.urls')),
     path('product/', include('product.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
